18612dc2e817d5d0554ac42d50d3982e3f8e4346
jamaspy <aspin@me.com>
2023-06-24 13:32:59 +1000

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,76 @@
-import Image from 'next/image'
-
+import { TfiLineDotted, TfiLayoutColumn4 } from "react-icons/tfi";
+import { CiMenuBurger } from "react-icons/ci";
+import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="min-h-screen bg-white relative flex flex-col">
+      <Navbar />
+      <div className="flex flex-col md:flex-row pl-1">
+        <div className="w-56 h-56 mx-auto md:mx-0 md:w-3/5 md:min-h-[75vh] md:rounded-tr-lg md:rounded-br-lg rounded-full relative bg-[linear-gradient(to_right_top,rgba(30,73,107,0.6),rgba(255,255,255,1)),url('/pool.jpg')] bg-cover bg-center opacity-60 shadow-xl">
+          <div className="h-full w-full flex-1 flex flex-col items-center justify-between">
+            <div className="w-full pl-40 flex flex-row items-start justify-start mt-12 pr-8" />
+            <p className=" nt-12 tracking-widest -rotate-90 opacity-100 font-semibold text-5xl text-primary [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] mr-56 hidden md:inline-block">
+              Togther
+            </p>
+            <div className="w-full pl-4 flex flex-row items-end justify-end mb-12 pr-8">
+              <TfiLayoutColumn4 className="text-6xl text-white opacity-60 hidden md:inline-block" />
+            </div>
+          </div>
+        </div>
+        <div className="mt-8 lg:mt-0 w-full lg:w-2/5 flex flex-col items-center justify-center lg:items-ends lg:justify-end relative">
+          <TfiLineDotted className="text-6xl text-primary  opacity-60 absolute top-10 left-4 -ml-12 -mt-12 hidden md:inline-block" />
+          <TfiLineDotted className="text-6xl text-primary  opacity-60 absolute top-12 left-4 -ml-12 -mt-12 hidden md:inline-block" />
+          <TfiLineDotted className="text-6xl text-primary  opacity-60 absolute top-14 left-4 -ml-12 -mt-12 hidden md:inline-block" />
+          <p className="text-4xl text-primary font-thin absolute top-1/2 -left-0 transform -translate-x-1/2 -translate-y-1/2 hidden lg:inline-block">
+            Building a stronger community to raise awareness of youth mental
+            health issues
+          </p>
+          <p className="px-4 text-slate-500 text-lg lg:text-justify pb-12">
+            <span className="font-semibold mb-2 block lg:hidden">
+              Building a stronger community to raise awareness of youth mental
+              health issues
+            </span>
+            This platform is a digital town hall where we are looking to
+            collect, inform and communicate the critical need of our community
+            to deal with the growing youth mental health issues leading to an
+            unacceptable level of suicide in our northern beaches community .
+          </p>
+          {/* <TfiArrowTopLeft className="text-6xl text-primary opacity-60 absolute bottom-10 right-4 ml-12 -mt-12 lg:hidden inline-block" /> hover:text-primary */}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      {/* 
+      <div className="w-full flex flex-row items-center justify-center z-10  absolute inset-0">
+        <p className="text-4xl text-primary font-light">
+          Building A Stronger Community
+        </p>
+      </div> */}
+      <div className="w-full flex flex-col md:flex-row mt-16 px-4">
+        <div className="flex-1">
+          <p className="text-xl">Who we are</p>
+          <p>
+            We are a concerned group of local northern beaches residents with a
+            broad range of backgrounds and skills that wish to address this dire
+            need to work towards zero death through this devastating growing
+            number of suicides. We strive to be a resource for information and
+            support for those in need.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+        </div>
+        <div className="flex-1">
+          <p>Title</p>
+          <p>
+            Labore irure cupidatat est sint. Voluptate anim non duis
+            reprehenderit officia esse amet ad et adipisicing. Quis ea laborum
+            eu est consequat excepteur est cupidatat nulla in fugiat fugiat
+            reprehenderit. Sunt qui enim consequat fugiat est cillum velit minim
+            aliquip ex aliquip cupidatat minim sunt. Tempor et duis elit duis
+            consequat. Commodo qui fugiat quis proident Lorem anim cupidatat est
+            ad proident. Commodo amet reprehenderit fugiat laborum officia irure
+            proident et nisi sit.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
       </div>
     </main>
-  )
+  );
 }
+
+//  bg-[linear-gradient(to_right_top,rgba(30,73,107,0.6),rgba(255,255,255,1)),url('/pool.jpg')] bg-cover bg-center
